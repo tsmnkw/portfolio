1dbcdb8a8f4bbf3fb52f5eed5ea1c203b8731fe5
tsmnkw <tsmnkw@proton.me>
2026-03-19 16:26:43 +1100

    title updates

--- src/lib/store.js
+++ src/lib/store.js
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store';
 
 export let nav = writable([
     {name: 'Nav', visible: false},
-    {name: 'Tasman\'s Portfolio', visible: true},
+    {name: 'Tasman', visible: true},
     {name: 'Web app projects', visible: false},
     {name: 'Website projects', visible: false},
     {name: 'Design portfolio', visible: false},
